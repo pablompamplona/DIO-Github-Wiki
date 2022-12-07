a4c481bf65cbe9250336a5eb07a272308c6772ea
pablompamplona <pablompamplona@gmail.com>
2022-12-07 20:02:04 -0300

    first commit

---
+++ src/components/Button/styles.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+export const ButtonContainer = styled.div`
+    background-color: #FAFAFA;
+    border-radius: 20px;
+    color: #22272e;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+
+
+    height: 62px;
+    width:80%;
+    margin: 20px;
+
+    &:hover {
+        cursor: pointer;
+        background-color: #FAFAFA40;
+    }
+`
---
+++ src/components/ItemRepo/styles.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+export const ItemContainer = styled.div`
+    width: 80%;
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p {
+        font-size: 16px;
+        color: #FAFAFA60;
+        margin-bottom: 10px;
+    }
+
+    a.remover {
+        color: #FF0000;
+        margin-left: 10px;
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 10px 0;
+
+    }
+`
